3c7b2dde5865017453fb930c0da00e7744a16d89
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-11-08 23:49:15 +0300

    Update F.py

--- HW1/F/F.py
+++ HW1/F/F.py
@@ -1,3 +1,8 @@
 def solution(n):
-    ### YOUR CODE ###
-    return
+    step_two = []
+    numb = 1
+    while num <= n:
+        step_two.append(num)
+        num = num * 2
+    return step_two
+ 
