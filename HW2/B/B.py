6c1bfc88e93fb3afb0fb997dcbe1ff4a219ce1ba
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-11-22 11:31:14 +0300

    Update B.py

--- HW2/B/B.py
+++ HW2/B/B.py
@@ -1,3 +1,12 @@
 def solution(x):
-    ### YOUR CODE ###
+    if i % 3:
+        
     return
+
+x = 'In the hole in the ground there lived 1 hobbit'
+
+'''
+% 3
+h - H .upper
+1 - one .replace()
+'''
