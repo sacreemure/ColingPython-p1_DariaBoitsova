6b7a3a98cab96510ddaefaa2d408ee24acd85e36
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-11-30 21:03:27 +0300

    Update task3.py

--- HW3/task3.py
+++ HW3/task3.py
@@ -1,1 +1,26 @@
+from typing import List
 
+class UnigramMorphAnalyzer:
+  def __init__(self):
+    self.stats = {}
+    
+  def train(self, X, y):
+    for word in X:
+      for i in range(4, 0, -1):
+        ending = word[-1:]
+        if word not in self.stats:
+          if ending not in self.stats:
+            self.stats[ending] = {}
+           
+          if y not in self.stats[y]
+  
+  def predict(self, X: List[str]):
+    return y
+  
+  def eval(self, X, y):
+    return score
+  
+if __name__ == "__main__":
+  tagger = UnigramMorphAnalyzer()
+  
+  tagger.train(X=["прыгать", "яблоко"], y=["NOUN", "VERB"])
