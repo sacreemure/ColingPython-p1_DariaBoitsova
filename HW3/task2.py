eff801b0efcbf1c7eb7d0a94e0d5743b1ce75b3a
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-11-30 20:26:11 +0300

    Update task2.py

--- HW3/task2.py
+++ HW3/task2.py
@@ -1,1 +1,20 @@
+import nltk
 
+class FileReader:
+  def __init__(self, path):
+    self.path = path
+    
+  def read(self):
+    return ""
+  
+  def count(self):
+    l_count = 0
+    w_count = 0
+    for line in open(self.path):
+      # returns an iterator
+      l_count += 1
+      w_count += len(nltk.word_tokenize(line))
+  def __add__(self, other):
+    return FileReader("")
+  
+FileReader("a.txt") + FileReader("b.txt")
