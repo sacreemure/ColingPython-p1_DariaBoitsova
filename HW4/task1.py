b17af6573d1d0de35988bff1d81a7ad3e2187c5d
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-08 20:17:52 +0300

    Update task1.py

--- HW4/task1.py
+++ HW4/task1.py
@@ -1,12 +1,26 @@
 class SpaceObject:
-  
-  class Planet:
-    Earth
+    pass
+
+class Planet:
+    earth = Planet('Earth')
+
+    def __init__(self, name, population=None):
+        self.name = name
+        self.population = population or []
+
 class Animal:
-  class Cat:
-    
-  class Raccoon:
-    
-  class Echhörnchen:
-    
-    
+
+
+class Cat(Animal):
+    def __init__(self,):
+
+
+class Dog(Animal):
+    def __init__(self,):
+
+
+class Chicken(Animal):
+    def __init__(self,):
+
+
+print(earth)
