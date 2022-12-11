6f96cd16cfe9cf8bbf6c6e09494db04163591e95
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-11 18:15:06 +0300

    Update C.py

--- HW2/C/C.py
+++ HW2/C/C.py
@@ -1,3 +1,8 @@
 def solution(arr):
-    ### YOUR CODE ###
-    return
+    newarr = []
+    
+    while arr:
+        newarr.append(arr.pop(0))
+        arr = list(map(list, zip(*arr)))[::-1]
+        
+    return [j for i in newarr for j in i]
