3fc30bd0576c43886b24c3431edc351588656c8d
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-11 15:27:18 +0300

    Update A.py

--- HW2/A/A.py
+++ HW2/A/A.py
@@ -1,7 +1,13 @@
 def solution(arr):
-    for i in arr:
-        if :
-            
+    max_len = 1
+    count = 1
+    for i in range(1, len(arr)):
+        if arr[i] == arr[i - 1]:
+            count += 1
+            if count > max_len:
+                max_len = count
+        else:
+            count = 1
         
     return max_len
 
