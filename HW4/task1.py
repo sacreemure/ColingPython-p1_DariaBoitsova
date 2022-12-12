e5bdad924e899b70920b67279dc0e445f52ffb0e
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-12 11:51:10 +0300

    Update task1.py

--- HW4/task1.py
+++ HW4/task1.py
@@ -1,7 +1,9 @@
 class SpaceObject:
-    pass
+    def __init__(self, name=None):
+        self.name = name
+    
 
-class Planet:
+class Planet(SpaceObject):
     earth = Planet('Earth')
 
     def __init__(self, name, population=None):
@@ -9,18 +11,24 @@ class Planet:
         self.population = population or []
 
 class Animal:
-
+    def __init__(self, name=None,
+        self.name = name
+                 
+    def __str__(self):
+       return self.name             
 
 class Cat(Animal):
     def __init__(self,):
-
+        self.name = name
 
 class Dog(Animal):
     def __init__(self,):
+        self.name = name         
 
 
 class Chicken(Animal):
     def __init__(self,):
+        self.name = name         
 
 
 print(earth)
