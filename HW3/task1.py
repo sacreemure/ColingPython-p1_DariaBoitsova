4913f04146162db8940bff38e44d0e2545b4fc1e
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-15 14:04:58 +0300

    Update task1.py

--- HW3/task1.py
+++ HW3/task1.py
@@ -1,10 +1,12 @@
 class OneIndexedList:
   
-  def __init__(self):
-    self.arr = arr
+  def __init__(self, items: list):
+    self.items = [] or items
     
-  def __setitem__(self):
+  
+  def __getitem__(self, index: int):
+    return self.items[index - 1]  
     
-  def __getitem__(self):
-    
+  def __setitem__(self, index: int, value: any):
+    self.items[index - 1] = value
     
