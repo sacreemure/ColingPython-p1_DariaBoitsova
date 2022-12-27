6d7171da4706d4cd59e25b41c32543d28caa2ce0
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-27 19:13:36 +0300

    Update task1.py

--- HW4/task1.py
+++ HW4/task1.py
@@ -4,31 +4,70 @@ class SpaceObject:
     
 
 class Planet(SpaceObject):
-    earth = Planet('Earth')
-
     def __init__(self, name, population=None):
-        self.name = name
+        super().__init__(name)
         self.population = population or []
+    
+    def __str__(self):
+        return f'Population of {self.name} = {self.population}
+
 
 class Animal:
-    def __init__(self, name=None,
+    def __init__(self, name=None, color=None, food=10)
         self.name = name
-                 
+        self.color = color
+        self.food = food
+         
     def __str__(self):
-       return self.name             
+       return self.name
 
+    def add(self):
+        self.population += 1
+    
+    def feed(self):
+        self.food += 1
+        return "I'm full"
+
+    
 class Cat(Animal):
-    def __init__(self,):
+    def __init__(self, name=None, color=None, food=2):
         self.name = name
+        self.color = color
+        self.food = food
+        
+    
+    def sound(self):
+        return f'{self.name}: мяу!'
 
+    
 class Dog(Animal):
-    def __init__(self,):
-        self.name = name         
+    def __init__(self, name=None, color=None, food=5):
+        self.name = name
+        self.color = color
+        self.food = food
+        
+       
+    def bark(self):
+        return f'{self.name}: гав гав'
 
 
 class Chicken(Animal):
-    def __init__(self,):
-        self.name = name         
+    def __init__(self, name=None, color=None, food=1):
+        self.name = name
+        self.color = color
+        self.food = food
+        
+    
+    def cluck(self):
+        return f'{self.name}: ко ко ко'
 
+ 
+
+earth = Planet('Earth')
+
+cats = [Cat('Musya', 'white'), Cat('Vasya', 'black')]
+dogs = [Dog('Bobik', 'brown')]
+chickens = [Chicken('Valya', 'yellow'), Chicken('Katya', 'grey')]
 
 print(earth)
+
