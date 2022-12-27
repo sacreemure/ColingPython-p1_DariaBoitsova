a012dd2c50f7855df8247a0dec4439e63ee86b40
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-27 17:25:14 +0300

    Update task2.py

--- HW4/task2.py
+++ HW4/task2.py
@@ -1,3 +1,20 @@
-class ReverseIter():
-  def __init__(self):
-    
+class ReverseIter(object):
+
+    def __init__(self, items: list) -> None:
+        self.items = items
+        self.num = len(items) - 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        num = self.num
+        self.num -= 1
+        return self.items[num]
+      
+it = ReverseIter([10, 54, 12, 0])
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
