826acc5a5817dc49a9be4f6bfba66ea9a9cc71a0
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-28 13:46:38 +0300

    Update hw6_task.py

--- HW6/hw6_task.py
+++ HW6/hw6_task.py
@@ -1,1 +1,15 @@
+import requests
 
+class GitHubUser():
+
+    def __init__(self, username):
+      self.username = username
+      
+    def get_rep(self):
+    
+    def get_lang(self):
+      
+    def get_maxrep():
+    
+    
+      
