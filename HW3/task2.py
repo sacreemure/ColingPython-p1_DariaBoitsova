32422f628d4b7558018efca4a5d43bdcb5627c5e
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-29 15:25:57 +0300

    Update task2.py

--- HW3/task2.py
+++ HW3/task2.py
@@ -3,22 +3,42 @@ from nltk import word_tokenize
 class FileReader:
   def __init__(self, path):
     self.path = path
+  
+  
+   def __add__(self, other):
+      newfile = open('addresult.txt')
+      with open(newfile, 'w') as i:
+        for line in i:
+          newfile.write(line)
+      with open(other.path, 'w') as f:
+        for line in f:
+          newfile.write(line)
+    return FileReader(newfile)
+
     
+  def __str__(self):
+    return f'{self.path}'
+  
+  
   def read(self):
-    return ""
+    with open(self.path) as read_file:
+      return read_file.read()
+    except FileNotFoundError:
+      return ''
   
   def write(self):
-  
+    with open(self.path, 'w') as write_file:
+      write_file.write()
+    except FileNotFoundError:
+      return ''
+    
   def count(self):
     l_count = 0
     w_count = 0
     for line in open(self.path):
-      # returns an iterator
       l_count += 1
       w_count += len(nltk.word_tokenize(line))
-      
-  def __add__(self, other):
-    return FileReader("")
-  
-FileReader("a.txt") + FileReader("b.txt")
+    return l_count, w_count
+ 
+
 
