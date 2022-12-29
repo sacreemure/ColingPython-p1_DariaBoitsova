85775a853e40e4f75415ed2b9be45d171c1b2745
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-29 17:23:21 +0300

    Update task3.py

--- HW3/task3.py
+++ HW3/task3.py
@@ -1,27 +1,41 @@
 import pickle
 from tqdm import tqdm
 
-class UnigramMorphAnalyzer:
-  def __init__(self):
-    self.stats = {}
-    
-  def train(self, X, y):
-    for word in X:
-      for i in range(4, 0, -1):
-        ending = word[-1:]
-        if word not in self.stats:
-          if ending not in self.stats:
-            self.stats[ending] = {}
-           
-          if y not in self.stats[y]
-  
-  def predict(self, X: List[str]):
-    return y
-  
-  def eval(self, X, y):
-    return score
-  
-if __name__ == "__main__":
-  tagger = UnigramMorphAnalyzer()
-  
 
+class UnigramMorphAnalyzer:
+
+    def __init__(self):
+        self.stats = {}
+        self.train_data = []
+        self.eval_data = []
+        file_name = 'pos_data.txt'
+
+    def __getitem__(self, token):
+        return self.stats[token]
+
+    def save(self):
+      with open('p_statistics.pkl', 'wb') as file:
+        pickle.dump(self.stats, file)
+
+    def load(self):
+      with open('p_statistics.pkl', 'rb') as file:
+        self.stats = pickle.load(file)
+        return self.stats
+
+    def train(self, x, y):
+       
+
+    def predict(self, token: str):
+       
+
+    def eval(self, x=None, y=None):
+        
+
+if __name__ == '__main__':
+    tagger = UnigramMorphAnalyzer()
+    tagger.train()
+    tagger.save()
+    tagger.load()
+    print(tagger.predict('спать'))
+    print(tagger['ть'])
+    print(tagger.eval())
