9cda4b29aa01d0f66fccc0a83f809e53b514ee2e
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-29 16:27:45 +0300

    Update task1.py

--- HW4/task1.py
+++ HW4/task1.py
@@ -6,58 +6,56 @@ class SpaceObject:
 class Planet(SpaceObject):
     def __init__(self, name, population=None):
         super().__init__(name)
-        self.population = population or []
+        self.population = population or 0
     
     def __str__(self):
         return f'Population of {self.name} = {self.population}
 
 
 class Animal:
-    def __init__(self, name=None, color=None, food=10)
+    def __init__(self, name=None, planet=None)
         self.name = name
-        self.color = color
-        self.food = food
+        self.planet = planet
          
-    def __str__(self):
-       return self.name
-
     def add(self):
         self.population += 1
-    
-    def feed(self):
-        self.food += 1
-        return "I'm full"
 
     
 class Cat(Animal):
-    def __init__(self, name=None, color=None, food=2):
-        self.name = name
-        self.color = color
-        self.food = food
+    def __init__(self, name, planet):
+        super().__init__(name, planet)
+        super().creation(planet)
+        self.food = 5
         
+    def feed(self):
+        self.food += 1
     
     def sound(self):
         return f'{self.name}: мяу!'
 
     
 class Dog(Animal):
-    def __init__(self, name=None, color=None, food=5):
-        self.name = name
-        self.color = color
-        self.food = food
+    def __init__(self, name, planet):
+        super().__init__(name, planet)
+        super().creation(planet)
+        self.food = 10
         
-       
+    def feed(self):
+        self.food += 5
+        
     def bark(self):
         return f'{self.name}: гав гав'
 
 
 class Chicken(Animal):
-    def __init__(self, name=None, color=None, food=1):
-        self.name = name
-        self.color = color
-        self.food = food
+    def __init__(self, name, planet):
+        super().__init__(name, planet)
+        super().creation(planet)
+        self.food = 2
         
-    
+    def feed(self):
+        self.food += 2
+        
     def cluck(self):
         return f'{self.name}: ко ко ко'
 
@@ -65,9 +63,9 @@ class Chicken(Animal):
 
 earth = Planet('Earth')
 
-cats = [Cat('Musya', 'white'), Cat('Vasya', 'black')]
-dogs = [Dog('Bobik', 'brown')]
-chickens = [Chicken('Valya', 'yellow'), Chicken('Katya', 'grey')]
+cats = [Cat('Musya', earth), Cat('Vasya', earth)]
+dogs = [Dog('Bobik', earth)]
+chickens = [Chicken('Valya', earth), Chicken('Katya', earth)]
 
 print(earth)
 
