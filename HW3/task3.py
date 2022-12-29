611d4c6b82ed31968aa1311beeffe8aff8d170d3
Daria Boitsova <116939976+sacreemure@users.noreply.github.com>
2022-12-29 18:00:50 +0300

    Update task3.py

--- HW3/task3.py
+++ HW3/task3.py
@@ -1,5 +1,7 @@
 import pickle
+from typing import List
 from tqdm import tqdm
+from corus import load_corpora
 
 
 class UnigramMorphAnalyzer:
@@ -10,8 +12,8 @@ class UnigramMorphAnalyzer:
         self.eval_data = []
         file_name = 'pos_data.txt'
 
-    def __getitem__(self, token):
-        return self.stats[token]
+    def __getitem__(self, x):
+        return self.stats[x]
 
     def save(self):
       with open('p_statistics.pkl', 'wb') as file:
@@ -23,17 +25,46 @@ class UnigramMorphAnalyzer:
         return self.stats
 
     def train(self, x, y):
-       
+        for word in x:
+            for i in range(4, 0, -1):
+                ending = word[-i:]
+                if word not in self.stats:
+                    if ending not in self.stats:
+                        self.stats[ending] = {}
+                    elif tag not in self.stats[y]:
+                        self.stats[ending][tag] = 1
+                    else:
+                        self.stats[ending][tag] += 1
+        return stats   
 
-    def predict(self, token: str):
+    def predict(self, x: List[str]):
+        if not self.stats:
+            stats = self.load()
+        else:
+            stats = self.stats
+        ending = x[-min(len(x), 4):]
+        try:
+            total = sum(stats[ending].values())
+            final_stats = {}
+            for tag, value in stats[ending].items():
+                probability = value / total
+                final_stats[tag] = probability
+        except KeyError:
+            return {0: 0}
+        return dict(sorted(final_stats.items(), key=lambda item: item[1], reverse=True))
        
 
     def eval(self, x, y):
-        
+        correct = 0
+        for word, tag in zip(x, y):
+            pred = self.predict(word)
+            if word == tag:
+                correct += 1
+        return correct / len(y)
 
 if __name__ == '__main__':
     tagger = UnigramMorphAnalyzer()
-    tagger.train()
+    #tagger.train()
     tagger.save()
     tagger.load()
     print(tagger.predict('спать'))
